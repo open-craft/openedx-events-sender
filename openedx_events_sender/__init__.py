880ade35ceb81486766b8a4368e021b2baa85960
Agrendalath <piotr@surowiec.it>
2022-09-29 15:26:46 +0200

    feat: initial commit
    Generated with `edx-cookiecutters`.

---
+++ openedx_events_sender/__init__.py
@@ -0,0 +1,7 @@
+"""
+Send Open edX events to selected URLs.
+"""
+
+__version__ = '0.1.0'
+
+default_app_config = 'openedx_events_sender.apps.OpenedxEventsSenderConfig'  # pylint: disable=invalid-name
---
+++ openedx_events_sender/apps.py
@@ -0,0 +1,13 @@
+"""
+openedx_events_sender Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class OpenedxEventsSenderConfig(AppConfig):
+    """
+    Configuration for the openedx_events_sender Django application.
+    """
+
+    name = 'openedx_events_sender'
